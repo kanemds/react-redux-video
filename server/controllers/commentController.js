057cc2a37aa40770a9b8d6469618be17e64e7919
Kanem <kanemdeng@email.com>
2022-10-20 14:30:54 -0700

    able to add comments

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,14 +1,44 @@
 const Comment = require('../models/comment');
+const Video = require('../models/video');
+const createError = require('../utility/error');
 
-const getAllRequest = async(req, res) => {
-  
+
+const addComment = async(req, res, next) => {
+  const newComment = new Comment({userId:req.user.id, ...req.body});
   try {
-    const getAll = await Comment.find();
-    res.status(200).json(getAll);
+    
+    const savedComment =  newComment.save();
+
+    res.status(200).josn(savedComment);
   } catch (error) {
-    console.log(error);
-    res.status(500).json(error.message);
+    next(error);
   }
 };
 
-module.exports = {getAllRequest};
+const deleteComment = async(req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(req.params.id);
+
+    // check if comment from same user or owner of the video
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleted Comment");
+    } else {
+      return next(createError(403, "Not Authenticated"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getComment = async(req, res, next) => {
+  try {
+    const comments = await Comment.find({videoId: req.params.videoId});
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {addComment, deleteComment, getComment};
