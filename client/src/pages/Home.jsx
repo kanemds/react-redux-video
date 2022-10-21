ce8f05572c50b1849d1b2668d8ab8c7a20a8c84d
Kanem <kanemdeng@email.com>
2022-10-20 18:55:43 -0700

    first feathing data from frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = ({type}) => {
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const getVideos =  async () => {
+      const res = await axios.get(`/video/${type}`)
+      setVideos(res.data)
+    }
+    getVideos()
+  },[type])
+
+  return (
+    <Container>
+      {videos.map(video => 
+      <Card />
+      )}
+    </Container>
+  );
+};
+
+export default Home;
