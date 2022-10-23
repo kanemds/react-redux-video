a7ed6aeba360f3f0ae7a9c3a84bf1732b67a1c4e
Kanem <kanemdeng@email.com>
2022-10-23 11:58:23 -0700

    each comment will have its user avatar image also created date

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { format } from 'timeago.js';
 
 const Container = styled.div`
   display: flex;
@@ -35,19 +37,31 @@ const Text = styled.span`
   font-size: 14px;
 `;
 
-const Comment = () => {
+const Comment = ({comment}) => {
+
+  const [commentUser, setCommentUser] = useState({})
+
+  useEffect(() => {
+    const fetchComment = async() => {
+      try {
+        const res = await axios.get(`/user/find/${comment.userId}`)
+        setCommentUser(res.data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    fetchComment()
+  },[comment.userId])
+
   return (
     <Container>
-      <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+      <Avatar src={commentUser.image} />
       <Details>
         <Name>
-          John Doe <Date>1 day ago</Date>
+          {commentUser.name} <Date>{format(comment.createAt)}</Date>
         </Name>
         <Text>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, ex
-          laboriosam ipsam aliquam voluptatem perferendis provident modi, sequi
-          tempore reiciendis quod, optio ullam cumque? Quidem numquam sint
-          mollitia totam reiciendis?
+         {comment.desc}
         </Text>
       </Details>
     </Container>
